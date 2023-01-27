70ed720327dd92531b0ec2fcbdf79279f9c55556
zxc88kr <zxc88kr@naver.com>
2023-01-28 00:09:14 +0900

    Conditionals

--- src/app.js
+++ src/app.js
@@ -1,5 +1,18 @@
-const h1 = document.querySelector("h1");
+function handleResize() {
+  document.querySelector("h2").style.color = "#ffffff";
+  const wframe = window.innerWidth;
 
-h1.addEventListener("click", () => {
-    h1.classList.toggle("clicked");
-});
+  if (wframe > 1200) {
+    document.body.style.backgroundColor = "#e74c3c";
+  } else if (wframe > 900) {
+    document.body.style.backgroundColor = "#f39c12";
+  } else if (wframe > 600) {
+    document.body.style.backgroundColor = "#9b59b6";
+  } else if (wframe > 300) {
+    document.body.style.backgroundColor = "#3498db";
+  } else {
+    document.body.style.backgroundColor = "#1abc9c";
+  }
+}
+
+window.addEventListener("resize", handleResize);
