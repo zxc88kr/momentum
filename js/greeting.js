10fc12c3353f7ce2b6751964b71c9a420007b39a
zxc88kr <zxc88kr@naver.com>
2023-02-01 03:56:58 +0900

    Number Game

--- js/greeting.js
+++ js/greeting.js
@@ -6,23 +6,23 @@ const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
 function handleLoginSubmit(event) {
-  event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  localStorage.setItem(USERNAME_KEY, loginInput.value);
-  paintGreetings();
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, loginInput.value);
+    paintGreetings();
 }
 
 function paintGreetings() {
-  const username = localStorage.getItem(USERNAME_KEY);
-  greeting.innerText = `Hello ${username}`;
-  greeting.classList.remove(HIDDEN_CLASSNAME);
+    const username = localStorage.getItem(USERNAME_KEY);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
-  loginForm.classList.remove(HIDDEN_CLASSNAME);
-  loginForm.addEventListener("submit", handleLoginSubmit);
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", handleLoginSubmit);
 } else {
-  paintGreetings();
+    paintGreetings();
 }
