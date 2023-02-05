d331c6847444b641bb9fab00c2148ca891b68ad7
zxc88kr <zxc88kr@naver.com>
2023-02-06 05:50:11 +0900

    Add Image

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const images = [ "0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png" ];
+const images = [ "0.png", "1.png", "2.png", "3.png", "4.png", "5.png", "6.png", "7.png", "8.jpg", "9.jpg", "10.jpg" ];
 
 const container = document.querySelector(".container");
 
